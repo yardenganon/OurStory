b33d65abe6db2ca4ccbeaed8bb3b27e1e6467172
fadi <frouhana@outlook.com>
2019-08-29 14:06:54 +0200

    memory,photo,video

--- src/main/java/com/example/ourstoryapp/domain/Memory.java
+++ src/main/java/com/example/ourstoryapp/domain/Memory.java
@@ -1,22 +1,21 @@
 package com.example.ourstoryapp.domain;
 
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.net.URI;
-import java.util.ArrayList;
 
-import javax.persistence.JoinColumn;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Memory {
@@ -48,6 +47,7 @@ public class Memory {
 	private Set<Tag> tags;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "memory")
+	@JsonIgnore
 	private List<Likes> likes;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "memory")
