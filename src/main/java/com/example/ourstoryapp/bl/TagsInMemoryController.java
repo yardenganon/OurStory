10b173680521504239b27393e9069d4282a1c4e1
Tarek <tsaleh550@gmail.com>
2019-08-30 21:13:07 +0300

    tagcontroller and tagsinmemorycontroller finished

--- src/main/java/com/example/ourstoryapp/bl/TagsInMemoryController.java
+++ src/main/java/com/example/ourstoryapp/bl/TagsInMemoryController.java
@@ -4,35 +4,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.ourstoryapp.da.MemTagRepository;
+import com.example.ourstoryapp.da.MemoryRepository;
 import com.example.ourstoryapp.da.TagRepository;
+
 import com.example.ourstoryapp.domain.Tag;
 
 @RestController 
 @RequestMapping("/tags")
-public class TagsController {
-
+public class TagsInMemoryController {
 	@Autowired
-    private TagRepository repository;
-
+    private MemTagRepository mem_tag_repository;
+	
 	//return all tags	
-	public Iterable<Tag> getTags() {
-		return repository.findAll();
-    } 
+	public Iterable<Tag> getmemTags() {
+		return mem_tag_repository.findAll();
+    }
+
+	
 	
 	//delete tag
-	public void delete(Tag tag) {
-		repository.delete(tag);
+	public void deletememtag(Tag tag) {
+		mem_tag_repository.delete(tag);
 	}
 	
 	//add tag
-	public void addtag(Tag tag) {
-		repository.save(tag);	
+	public void addmemtag(Tag tag) {
+		mem_tag_repository.save(tag);	
 	}
 	
 	// find tag by name
 	public void findbyid(String tag_name) {
-		repository.findById(tag_name);
+		mem_tag_repository.findById(tag_name);
 	}
 
-
+	
 }
