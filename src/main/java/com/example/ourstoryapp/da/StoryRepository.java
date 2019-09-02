e6e2665f7019c979dd674d77b3e93bac77d0786a
Yarden Ganon <yardenganon@gmail.com>
2019-09-02 15:13:30 +0300

    FindTop3TagsByStoryId

--- src/main/java/com/example/ourstoryapp/da/StoryRepository.java
+++ src/main/java/com/example/ourstoryapp/da/StoryRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.ourstoryapp.domain.Story;
@@ -14,7 +13,4 @@ public interface StoryRepository extends JpaRepository <Story ,Long> {
 
 	@Query(value = "SELECT STORY.* FROM STORY WHERE Story.name_of_person LIKE concat('%',?1,'%')", nativeQuery = true)
 	List<Story> findStoriesByKeyword(String name_of_person);
-
-
-
 }
