102942655af15e3bd9aeb502b18c453f0cb3d63d
Yarden Ganon <yardenganon@gmail.com>
2019-09-02 14:27:26 +0300

    Query - FindStoryByName

--- src/main/java/com/example/ourstoryapp/da/StoryRepository.java
+++ src/main/java/com/example/ourstoryapp/da/StoryRepository.java
@@ -2,18 +2,19 @@ package com.example.ourstoryapp.da;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.example.ourstoryapp.domain.Memory;
 import com.example.ourstoryapp.domain.Story;
 
 @RepositoryRestResource
-public interface StoryRepository extends CrudRepository <Story ,Long> { 
-	
-	
-	@Query(value = "SELECT STORY.* FROM STORY WHERE (Story.name_of_person LIKE CONCAT('%',?1,'%'))", nativeQuery = true)
-	List<Story> findStoryByKeyword(String name_of_person);
+public interface StoryRepository extends JpaRepository <Story ,Long> { 
+
+	@Query(value = "SELECT STORY.* FROM STORY WHERE Story.name_of_person LIKE concat('%',?1,'%')", nativeQuery = true)
+	List<Story> findStoriesByKeyword(String name_of_person);
+
+
 
 }
--- src/main/java/com/example/ourstoryapp/web/StoryController.java
+++ src/main/java/com/example/ourstoryapp/web/StoryController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -75,9 +76,10 @@ public class StoryController {
 	
 	//TODO findByKeyword find story by name
 	
-	@RequestMapping("/findStoryByKeyword/{name_of_person}")
-	public Iterable<Story> findStoryByKeyword(String name_of_person) {
-		return repository.findStoryByKeyword(name_of_person);
+	@RequestMapping("/findStoriesByKeyword")
+	public Iterable<Story> findStoriesByKeyword(@RequestParam("name") String name_of_person) {
+		return repository.findStoriesByKeyword(name_of_person);
+		//return repository.findStoriesByKeyword();
 	}
 	
 	//TODO findMemoryTagsByStoryId
