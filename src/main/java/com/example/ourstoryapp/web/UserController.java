637cb23eb4b97538a8aa673b7392e0f102f07f2e
fadi <frouhana@outlook.com>
2019-09-10 08:33:34 +0200

    changing mail into variable

--- src/main/java/com/example/ourstoryapp/web/UserController.java
+++ src/main/java/com/example/ourstoryapp/web/UserController.java
@@ -118,7 +118,7 @@ public class UserController {
 		updatePassword(u.getUser_id());
 		u = findByEmail(mail);
 		// don't forget to update to mail parameter
-		String recipient = "frouhana@outlook.com";
+		String recipient = mail;
 		String message = u.getPassword();
 		mailClient.prepareAndSend(recipient, message);
 	}
