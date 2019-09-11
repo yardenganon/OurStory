ba4685c8e41c5620e87d1b43b5319fcbc6706860
Yarden Ganon <yardenganon@gmail.com>
2019-09-11 11:31:17 +0300

    Fixed

--- src/main/java/com/example/ourstoryapp/domain/Memory.java
+++ src/main/java/com/example/ourstoryapp/domain/Memory.java
@@ -54,15 +54,15 @@ public class Memory {
 	private Set<Tag> tags;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "memory")
-	@JsonIgnore
+	
 	private List<Likes> likes;
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "memory")
-	@JsonIgnore
+	
 	private List<Picture> pictures;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "memory")
-	@JsonIgnore
+
 	private List<Video> videos;
 
 	public Memory() {
